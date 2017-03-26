fc512fd06bbc0c226cf1f0f1531f9917a8b5b438
John Shepler <john.sheplerjr@gmail.com>
2017-03-26 12:12:17 -0600

    cors install

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 6666
-// const cors = require('cors')
+const cors = require('cors')
 const house = require('./routes/house')
 const address = require('./routes/address')
 const tenant = require('./routes/tenant')
 const image = require('./routes/image')
 
 
-// app.use(cors())
-// app.use(cors({origin: 'https://grecipesfrontend.firebaseapp.com/'}))
+app.use(cors())
+// app.use(cors({origin: 'https://realestate-767c2.firebaseapp.com/'}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
