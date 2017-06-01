772d1924ecc9cbd5d96f38658b0a2268bf0afdc7
John Shepler <john.sheplerjr@gmail.com>
2017-06-01 13:48:48 -0600

    fix syntax

--- routes/house.js
+++ routes/house.js
@@ -16,6 +16,7 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', upload.single('image'),  (req, res) => {
+    console.log(req.body);
     database.createHouse(req.body)
         .then(() => {
             res.sendStatus(201)
