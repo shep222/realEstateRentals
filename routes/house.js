dd1ff50409dfa5eb947f2187569b1f820a529a41
John Shepler <john.sheplerjr@gmail.com>
2017-06-01 13:45:13 -0600

    fix syntax

--- routes/house.js
+++ routes/house.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const database = require('../database/houseAPI')
 const upload = require('multer')()
 
-
 router.get('/', (req, res) => {
     database.getHouse()
         .then((house) => {
@@ -27,9 +26,31 @@ router.post('/', upload.single('image'),  (req, res) => {
             myDate = myDate.toLocaleTimeString()
             s3.putObject({
               Bucket: process.env.S3_BUCKET,
-              Key: id,
+              Key: myDate,
               Body: new Buffer(req.file.buffer)
           }, (err,data) => {
               if (err) {
                 console.log(err);
+              } else {
+                // res.json(`{"success": true}`)
+                res.json(data);
+                console.log('made it');
+              }
+            })
+        })
+})
+
+router.patch('/:id', (req, res) => {
+    database.editHouse(req.params.id, req.body).then((id) => {
+        res.json(id)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    database.deleteHouse(req.params.id).then((id) => {
+        res.json(id)
+    })
+})
+
+
 module.exports = router
