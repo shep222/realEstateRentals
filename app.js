6979e9cd1893f09322641f97130036377a480132
John Shepler <john.sheplerjr@gmail.com>
2017-06-02 09:51:54 -0600

    back to 3 posts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 6666
 const cors = require('cors')
@@ -10,9 +10,8 @@ const image = require('./routes/image')
 
 app.use(cors())
 // app.use(cors({origin: 'https://realestate-767c2.firebaseapp.com/'}))
-// app.use(bodyParser.json())
-
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use('/house', house)
 app.use('/image', image)
