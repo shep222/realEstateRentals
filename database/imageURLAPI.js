5ca7345f808e53f42830f0f672deb94b575846ea
John Shepler <john.sheplerjr@gmail.com>
2017-06-02 14:17:46 -0600

    add third route

--- database/imageURLAPI.js
+++ database/imageURLAPI.js
@@ -1,9 +1,9 @@
 const db = require('./knex')
 
 module.exports = {
-  getImage: () => db('image'),
-  createImage: (newItem) => db('image').insert(newItem),
-  editImage: (id, newItem) => db('image').where('id', id).update(newItem, 'id'),
-  getSingleImage: (id) => db('image').where('id', id),
-  deleteImage: (id) => db('image').where('id', id).del().returning('id')
+  getImageURL: () => db('image'),
+  createImageURL: (newItem) => db('image').insert(newItem),
+  editImageURL: (id, newItem) => db('image').where('id', id).update(newItem, 'id'),
+  getSingleImageURL: (id) => db('image').where('id', id),
+  deleteImageURL: (id) => db('image').where('id', id).del().returning('id')
 }
