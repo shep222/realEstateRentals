b73ba5227ae0907bf3866e277c39665805a1237a
John Shepler <john.sheplerjr@gmail.com>
2017-06-02 08:54:27 -0600

    S3 garbage

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 6666
 const cors = require('cors')
@@ -12,7 +12,7 @@ app.use(cors())
 // app.use(cors({origin: 'https://realestate-767c2.firebaseapp.com/'}))
 // app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use('/house', house)
 app.use('/image', image)
