cd34289ecb402c52919a5a0ef87d08b24da37e3f
John Shepler <john.sheplerjr@gmail.com>
2017-06-02 14:26:26 -0600

    add third route

--- routes/imageURL.js
+++ routes/imageURL.js
@@ -2,33 +2,33 @@ const router = require('express').Router()
 const database = require('../database/imageURLAPI')
 
 router.get('/', (req, res) => {
-    database.getImage()
+    database.getImageURL()
         .then((house) => {
             res.send(house)
         })
 })
 
 router.get('/:id', (req, res) => {
-    database.getSingleImage(req.params.id).then((house) => {
+    database.getSingleImageURL(req.params.id).then((house) => {
         res.send(house)
     })
 })
 
 router.post('/', (req, res) => {
-    database.createImage(req.body)
+    database.createImageURL(req.body)
         .then(() => {
             res.sendStatus(201)
         })
 })
 
 router.patch('/:id', (req, res) => {
-    database.editImage(req.params.id, req.body).then((id) => {
+    database.editImageURL(req.params.id, req.body).then((id) => {
         res.json(id)
     })
 })
 
 router.delete('/:id', (req, res) => {
-    database.deleteImage(req.params.id).then((id) => {
+    database.deleteImageURL(req.params.id).then((id) => {
         res.json(id)
     })
 })
