ed681ab0417ab04d529594024b66bbe2470c6bcf
John Shepler <john.sheplerjr@gmail.com>
2017-06-12 11:40:28 -0600

    change image route

--- database/imageAPI.js
+++ database/imageAPI.js
@@ -1,9 +1,9 @@
 const db = require('./knex')
 
 module.exports = {
-  getImage: () => db('s3'),
-  createImage: (newItem) => db('s3').insert(newItem),
-  editImage: (id, newItem) => db('s3').where('id', id).update(newItem, 'id'),
-  getSingleImage: (id) => db('s3').where('id', id),
-  deleteImage: (id) => db('s3').where('id', id).del().returning('id')
+  getImage: () => db('image'),
+  createImage: (newItem) => db('image').insert(newItem),
+  editImage: (id, newItem) => db('image').where('id', id).update(newItem, 'id'),
+  getSingleImage: (id) => db('image').where('id', id),
+  deleteImage: (id) => db('image').where('id', id).del().returning('id')
 }
